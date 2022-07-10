9dbb12d06cf3862619012c6364bc2de4e4e55e4b
1010varun <varunagarwal10.2@gmail.com>
2022-07-11 00:51:16 +0530

    applied condition

--- count-app/src/App.js
+++ count-app/src/App.js
@@ -3,15 +3,16 @@ import "./style.css";
 
 const App = () => {
 
-    let [count, setCount] = useState(10);
+    let [count, setCount] = useState(50);
 
     return (
         <div>
             <h1>Welcome To Counter Application</h1>
             <p className="count">current value of count is {count}</p>
-            <button onClick = {() => setCount(count = 0)} className="button">reset count to zero</button><br></br><br></br>
-            <button onClick = {()=> setCount(count + 1)} className="button">increase count by 1</button><br></br><br></br>
-            <button onClick = {() => setCount(count - 1)} className="button">decrease conut by 1</button>
+            <button onClick = {() => setCount(count = 50)} className="button">reset count to fifty</button><br></br><br></br>
+            <button onClick = {()=> ((count >= 100) ? "" : setCount(count + 1))} className="button">increase count by 1
+            </button><br></br><br></br>
+            <button onClick = {() => ((count !== 0) ? setCount(count - 1) : "")} className="button">decrease conut by 1</button>
 
         </div>
     )
